354eaf9dce65612b9d4136e6ea547e7fc2dda983
Quentin <39061148+LapsTimeOFF@users.noreply.github.com>
2023-01-14 23:46:45 +0100

    Fix base

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from "vuepress";
+
+export default defineUserConfig({
+  lang: "en-US",
+  title: "F1MV_Integration_Docs",
+  description: "Get started with the F1MV API !",
+  base: '/F1MV_Integration_Docs/'
+});
