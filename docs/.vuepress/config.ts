cfb1853f1067f252070c071d6b6e95b75f60cb26
Quentin <39061148+LapsTimeOFF@users.noreply.github.com>
2023-01-15 00:04:58 +0100

    Start add docs, spelling check

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,14 @@
 import { defineUserConfig } from "vuepress";
+import { externalLinkIconPlugin } from "@vuepress/plugin-external-link-icon";
 
 export default defineUserConfig({
   lang: "en-US",
   title: "F1MV_Integration_Docs",
   description: "Get started with the F1MV API !",
-  base: '/F1MV_Integration_Docs/'
+  base: "/F1MV_Integration_Docs/",
+  plugins: [
+    externalLinkIconPlugin({
+      // options
+    }),
+  ],
 });
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,9 @@
 # Introduction
+
+If you are reading this, it probably means you want to learn how to create your own integration for the app MultiViewer for F1. Guess what! You aren't at the perfect place!
+
+Just kidding, here you will learn about the API.
+
+## Before you begin
+
+Alright, making an integration i
