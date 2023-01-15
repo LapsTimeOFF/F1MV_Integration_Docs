c2589324a4a454a9809bc0749a434e80c6ec3794
Quentin <39061148+LapsTimeOFF@users.noreply.github.com>
2023-01-15 10:51:57 +0100

    Update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,16 +3,22 @@ import { externalLinkIconPlugin } from "@vuepress/plugin-external-link-icon";
 import sidebar from "./sidebar";
 
 export default defineUserConfig({
-  lang: "en-US",
-  title: "F1MV_Integration_Docs",
-  description: "Get started with the F1MV API !",
-  base: "/F1MV_Integration_Docs/",
-  plugins: [
-    externalLinkIconPlugin({
-      // options
+    lang: "en-US",
+    title: "F1MV_Integration_Docs",
+    description: "Get started with the F1MV API !",
+    base: "/F1MV_Integration_Docs/",
+    plugins: [
+      externalLinkIconPlugin({
+        // options
+      }),
+    ],
+    theme: defaultTheme({
+      sidebar,
+      locales: {
+          "/F1MV_Integration_Docs/": {
+              editLinkText: "Edit this page on GitHub",
+              sidebar
+          },
+      },
     }),
-  ],
-  theme: defaultTheme({
-    sidebar
-  })
 });
