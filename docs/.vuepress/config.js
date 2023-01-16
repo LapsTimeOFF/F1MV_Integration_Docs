71de7a38ad934f72b817b13e569fd33fae85d027
Quentin <39061148+LapsTimeOFF@users.noreply.github.com>
2023-01-16 22:17:01 +0100

    Deploy new theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,10 @@
+const sidebar = require('./sidebar')
+
 module.exports = {
   theme: 'vt',
-  themeConfig: {},
+  themeConfig: {
+    enableDarkMode: true,
+    repo: "https://github.com/LapsTimeOFF/F1MV_Integration_Docs",
+    sidebar: sidebar
+  },
 };
--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,22 +1,24 @@
-export default {
-	'/': [
-		{
+module.exports = {
+	'/': [{
 			text: 'Home',
 			children: [
 				'/',
 			],
+			collapsable: false,
 		},
 		{
 			text: 'Plugin Developer role',
 			children: [
 				'Plugin Developer Role.md',
 			],
+			collapsable: false,
 		},
 		{
 			text: 'Sharing',
 			children: [
 				'Share.md',
 			],
+			collapsable: false,
 		},
 	],
 };
